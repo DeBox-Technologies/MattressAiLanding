dd0ee466792a18bd41dee9f7f486ca6b2410f401
max whitehead <fifes-catkin.0r@icloud.com>
2023-07-18 18:54:52 +0100

    Added Mailchimp Button

--- src/components/SignUpInput.jsx
+++ src/components/SignUpInput.jsx
@@ -1,54 +1,30 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 const SignupForm = () => {
-  const API_KEY = import.meta.env.VITE_APP_MAILCHIMP_API_KEY
-  const [email, setEmail] = useState('');
-
-  const handleInputChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post(
-        'https://us8.api.mailchimp.com/3.0/lists/<list_id>/members',
-        {
-          email_address: email,
-          status: 'subscribed',
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'apikey ${API_KEY}',
-          },
-        }
-      );
-
-      console.log(response.data); // Handle success
-
-      // Optionally, display a success message or redirect the user
-    } catch (error) {
-      console.error(error); // Handle error
-
-      // Optionally, display an error message
-    }
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        placeholder="Enter your email"
-        value={email}
-        onChange={handleInputChange}
-        className='text-gray-700 dark:-primary-50 bg-primary-50/70 dark:bg-gray-800/60 border-2 dark:border-secondary-50/70 rounded-lg px-4 py-2 w-80 md:w-96 focus:outline-none focus:ring-2 focus:ring-primary-200 dark:focus:ring-primary-100'
-      />
-      <button type="submit" className='bg-primary-200 hover:bg-primary-200/70 active:bg-primary-200/90  ml-2 py-2 px-4 border-2 border-primary-200 rounded-lg text-secondary-50'>Subscribe</button>
-    </form>
+    <>
+      <a
+        href="http://eepurl.com/isHTp-/" target='_blank'
+        className='lg:ml-10 bg-primary-200 hover:bg-green-500 active:bg-green-600 text-secondary-50 hover:shadow-green transition-all ease-in-out duration-300 p-2 px-8 rounded-xl lg:mt-8 mt-4'>
+        <div className='flex flex-row gap-2 justify-center items-center lg:text-xl'>
+          Subscribe
+          <svg
+            aria-hidden='true'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='1.5'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+            className='w-4 h-4 lg:w-6 lg:h-6 mt-1 hover:animate-pulse'>
+            <path
+              d='M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3'
+              strokeLinecap='round'
+              strokeLinejoin='round'></path>
+          </svg>
+        </div>
+      </a>
+    </>
   );
-};
+}
 
 export default SignupForm;
