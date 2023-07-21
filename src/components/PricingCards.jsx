d386492bd67f179bd5c361b961db9007dfcf8507
max whitehead <fifes-catkin.0r@icloud.com>
2023-07-21 14:39:07 +0100

    Fixed Responsivness Issues

--- src/components/PricingCards.jsx
+++ src/components/PricingCards.jsx
@@ -89,7 +89,7 @@ const PricingCards = () => {
                     clipRule='evenodd'></path>
                 </svg>
                 <span className='text-base font-normal leading-tight text-gray-500'>
-                  “Upload Inventory” Function
+                  Specific mattress recommendation from your “Inventory”
                 </span>
               </li>
               <li className='flex space-x-3 line-through decoration-gray-500'>
@@ -106,7 +106,7 @@ const PricingCards = () => {
                     clipRule='evenodd'></path>
                 </svg>
                 <span className='text-base font-normal leading-tight text-gray-500'>
-                  specific mattress recommendation from your “Inventory”
+                  30-minute setup for inventory function
                 </span>
               </li>
               <li className='flex space-x-3 line-through decoration-gray-500'>
@@ -141,23 +141,6 @@ const PricingCards = () => {
                 </svg>
                 <span className='text-base font-normal leading-tight text-gray-500'>
                   Full Customization (Company Logo, Branding, etc.)
-                </span>
-              </li>
-              <li className='flex space-x-3 line-through decoration-gray-500'>
-                <svg
-                  aria-hidden='true'
-                  className='flex-shrink-0 w-5 h-5 text-gray-400 dark:text-gray-500'
-                  fill='currentColor'
-                  viewBox='0 0 20 20'
-                  xmlns='http://www.w3.org/2000/svg'>
-                  <title>Check icon</title>
-                  <path
-                    fillRule='evenodd'
-                    d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                    clipRule='evenodd'></path>
-                </svg>
-                <span className='text-base font-normal leading-tight text-gray-500'>
-                  1 hour setup for branding & inventory
                 </span>
               </li>
               <li className='flex space-x-3 line-through decoration-gray-500'>
@@ -269,23 +252,6 @@ const PricingCards = () => {
                     clipRule='evenodd'></path>
                 </svg>
                 <span className='text-base font-normal leading-tight text-gray-500 dark:text-gray-400'>
-                  “Upload Inventory” Function
-                </span>
-              </li>
-              <li className='flex space-x-3'>
-                <svg
-                  aria-hidden='true'
-                  className='flex-shrink-0 w-5 h-5 text-primary-200 dark:text-primary-200'
-                  fill='currentColor'
-                  viewBox='0 0 20 20'
-                  xmlns='http://www.w3.org/2000/svg'>
-                  <title>Check icon</title>
-                  <path
-                    fillRule='evenodd'
-                    d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                    clipRule='evenodd'></path>
-                </svg>
-                <span className='text-base font-normal leading-tight text-gray-500 dark:text-gray-400'>
                   Specific mattress recommendations based on “Inventory”
                 </span>
               </li>
@@ -320,7 +286,7 @@ const PricingCards = () => {
                     clipRule='evenodd'></path>
                 </svg>
                 <span className='text-base font-normal leading-tight text-gray-500'>
-                  Full Customization (Company Logo, Branding, etc.)
+                  30-minute setup for inventory function
                 </span>
               </li>
               <li className='flex space-x-3 line-through decoration-gray-500'>
@@ -337,7 +303,7 @@ const PricingCards = () => {
                     clipRule='evenodd'></path>
                 </svg>
                 <span className='text-base font-normal leading-tight text-gray-500'>
-                  1 hour setup for branding & inventory
+                  Full Customization (Company Logo, Branding, etc.)
                 </span>
               </li>
               <li className='flex space-x-3 line-through decoration-gray-500'>
@@ -449,24 +415,7 @@ const PricingCards = () => {
                     clipRule='evenodd'></path>
                 </svg>
                 <span className='text-base font-normal leading-tight text-gray-500 dark:text-gray-400'>
-                  “Upload Inventory” Function
-                </span>
-              </li>
-              <li className='flex space-x-3'>
-                <svg
-                  aria-hidden='true'
-                  className='flex-shrink-0 w-5 h-5 text-primary-200 dark:text-primary-200'
-                  fill='currentColor'
-                  viewBox='0 0 20 20'
-                  xmlns='http://www.w3.org/2000/svg'>
-                  <title>Check icon</title>
-                  <path
-                    fillRule='evenodd'
-                    d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                    clipRule='evenodd'></path>
-                </svg>
-                <span className='text-base font-normal leading-tight text-gray-500 dark:text-gray-400'>
-                  specific mattress recommendation from your “Inventory”
+                  Specific mattress recommendation from your “Inventory”
                 </span>
               </li>
               <li className='flex space-x-3'>
@@ -500,7 +449,7 @@ const PricingCards = () => {
                     clipRule='evenodd'></path>
                 </svg>
                 <span className='text-base font-normal leading-tight text-gray-500 dark:text-gray-400'>
-                  Full Customization (Company Logo, Branding, etc.)
+                  30-minute setup for branding
                 </span>
               </li>
               <li className='flex space-x-3'>
@@ -517,7 +466,7 @@ const PricingCards = () => {
                     clipRule='evenodd'></path>
                 </svg>
                 <span className='text-base font-normal leading-tight text-gray-500 dark:text-gray-400'>
-                  1 hour setup for branding & inventory
+                  Full Customization (Company Logo, Branding, etc.)
                 </span>
               </li>
               <li className='flex space-x-3'>
@@ -545,26 +494,32 @@ const PricingCards = () => {
             </button>
           </div>
         </div>
-        <div className='flex justify-center items-center absolute top-0 right-4'>
-          <div className='bg-secondary-200/90 border border-gray-200 rounded-full shadow max-w-xs'>
-            <div className="flex flex-col justify-center items-center my-8 text-white">
-            <svg
-              className='w-12 h-12'
-              fill='none'
-              stroke='currentColor'
-              stroke-width='1.5'
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/svg'
-              aria-hidden='true'>
-              <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                d='M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z'></path>
-            </svg>
-            <p className='text-center p-4 text-white text-xs mx-6'>
-              Enterprise/E-Commerce<br/>
-              Contact for details: blake@themattressai.com
-            </p>
+      </div>
+      <div className='2xl:max-w-screen-2xl mx-auto'>
+        <div className='flex flex-row justify-center items-center gap-6 mt-24 bg-no-repeat bg-cover bg-[url("https://res.cloudinary.com/djr22sgp3/image/upload/v1688031512/bgBlur_wpeme9.webp")] mx-5 md:mx-20 py-14 rounded-xl mb-24'>
+          <div className='flex justify-center items-center top-0 right-4'>
+            <div className='bg-secondary-200/60 border border-gray-200 rounded-lg shadow max-w-xs'>
+              <div className='flex flex-col justify-center items-center my-8 text-white'>
+                <svg
+                  className='w-12 h-12 lg:w-24 lg:h-24'
+                  fill='none'
+                  stroke='currentColor'
+                  stroke-width='1.5'
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'
+                  aria-hidden='true'>
+                  <path
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    d='M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z'></path>
+                </svg>
+                <div className="flex flex-col justify-center items-center">
+                  <p className='text-center p-4 text-white text-s lg:text-xl font-semibold mx-6'>
+                    Enterprise/E-Commerce
+                  </p>
+                  <p className="text-center text-xs lg:text-lg">Contact for details: blake@themattressai.com</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
